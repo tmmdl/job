67311f36b60d3ea01c51b5a885ca9231cdd2a1eb
tamerlan <mainstream95@bk.ru>
2018-05-20 08:15:13 +0400

    prod conf fix

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "Start application.."
-java -jar -XX:MaxMetaspaceSize=128m  --spring.config.location=classpath:/applitcation.properties,classpath:/production.properties -Xmx1024m target/job-1.0-SNAPSHOT.jar
+echo "11 Start application.."
+java -jar  -XX:MaxMetaspaceSize=128m   -Xmx1024m target/job-1.0-SNAPSHOT.jar --spring.config.location=classpath:/application.properties,classpath:/production.properties
